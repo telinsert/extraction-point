0da2c384f74628edf745a2b5994e2d9e24e9fbb4
MelonG5 <edwardgg09@outlook.com>
2025-10-21 16:54:16 -0700

    Added zombie animations and fixed regen bug
    hello

--- Extraction Point 67/Assets/Scripts/Core/Health.cs
+++ Extraction Point 67/Assets/Scripts/Core/Health.cs
@@ -1,39 +1,39 @@
 // In /Scripts/Core/Health.cs
-using UnityEngine;
 using System;
+using UnityEngine;
+using UnityEngine.AI;
 
-// We REMOVED the [RequireComponent(typeof(PlayerStats))] attribute. This is the key change.
 public class Health : MonoBehaviour
 {
     [Header("Default Health Settings")]
+#pragma warning disable 0649
     [SerializeField] private int initialMaxHealth = 100;
+#pragma warning restore 0649
     public int MaxHealth { get; private set; }
 
     private int currentHealth;
-    private PlayerStats stats; // This will remain null for non-player objects.
+    private PlayerStats stats;
     private Animator animator;
+    private bool isDowned = false;
 
     public event Action OnDeath;
     public event Action<int, int> OnHealthChanged;
 
     void Awake()
     {
-        // Try to get the stats component. It's okay if this is null.
+        // --- THIS METHOD IS NOW CORRECTED ---
         stats = GetComponent<PlayerStats>();
         animator = GetComponent<Animator>();
 
-        // If we ARE on a player that has stats...
-        if (stats != null)
+        if (stats != null) // If this is a player
         {
-            // ...then our max health is determined by the stats component.
             MaxHealth = stats.maxHealth;
         }
-        else
+        else // If this is a Zombie or other non-player
         {
-            // Otherwise, use the default value set in the inspector.
+            // Restore the original, correct logic:
             MaxHealth = initialMaxHealth;
         }
-        // If stats is null (because we're on a Zombie), it will just use the maxHealth value set above.
     }
 
     void Start()
@@ -42,16 +42,17 @@ public class Health : MonoBehaviour
         OnHealthChanged?.Invoke(currentHealth, MaxHealth);
     }
 
-    // This method is now used to update health when a player gets an upgrade.
     public void UpdateMaxHealth(int newMaxHealth, int amountToHeal)
     {
         MaxHealth = newMaxHealth;
-        Heal(amountToHeal); // Give the health boost immediately
+        Heal(amountToHeal);
         OnHealthChanged?.Invoke(currentHealth, MaxHealth);
     }
 
     public void TakeDamage(int amount)
     {
+        if (isDowned) return;
+
         currentHealth -= amount;
         if (currentHealth <= 0)
         {
@@ -63,63 +64,65 @@ public class Health : MonoBehaviour
 
     public void Heal(int amount)
     {
+        if (isDowned) return;
         currentHealth = Mathf.Min(currentHealth + amount, MaxHealth);
         OnHealthChanged?.Invoke(currentHealth, MaxHealth);
     }
 
     void Die()
     {
+
         OnDeath?.Invoke();
-        // --- MODIFIED LOGIC START ---
-        // Check if this object is a player by seeing if it has PlayerStats
-        if (stats != null)
+
+        if (stats != null) // If this is a player
         {
-            // --- IT'S A PLAYER ---
-            // 1. Notify the GameManager that this player is down.
+            isDowned = true;
             GameManager.Instance.OnPlayerDowned(stats.playerNumber);
-            // 2. Disable player components to stop them from moving or shooting.
             GetComponent<PlayerController>().enabled = false;
             GetComponent<CharacterController>().enabled = false;
-            gameObject.tag = "DownedPlayer"; // Change the tag so zombies will ignore this player.
+            gameObject.tag = "DownedPlayer";
 
-            if (animator != null) // Tell the animator to play the downed animation
+            if (animator != null)
             {
                 animator.SetTrigger("IsDowned");
             }
-
             Debug.Log($"Player {stats.playerNumber} is downed.");
         }
-        else
+        else // If this is an Enemy
         {
-            // --- IT'S AN ENEMY OR OTHER OBJECT ---
-            // Use the original behavior: destroy it.
-            Destroy(gameObject);
+            if (GetComponent<ZombieAI>() != null)
+            {
+                // It's a ZOMBIE. Use the animation and delay.
+                if (animator != null) animator.SetTrigger("Die");
+                if (GetComponent<NavMeshAgent>() != null) GetComponent<NavMeshAgent>().enabled = false;
+                if (GetComponent<Collider>() != null) GetComponent<Collider>().enabled = false;
+                GetComponent<ZombieAI>().enabled = false; // Disable the AI script itself.
+
+                Destroy(gameObject, 0.65f);
+            }
+            else
+            {
+                // It's a SPAWNER or some other destructible object.
+                Destroy(gameObject);
+            }
+
         }
-        // --- MODIFIED LOGIC END ---
     }
 
-    // --- NEW METHOD START ---
     public void Revive(float healthPercentage)
     {
-        // 1. Re-enable player components
+        isDowned = false;
         GetComponent<PlayerController>().enabled = true;
         GetComponent<CharacterController>().enabled = true;
-
-        // 2. Change the tag back to "Player" so zombies can target them again.
         gameObject.tag = "Player";
-
-        // 3. Heal for a percentage of max health.
         int healthToRestore = Mathf.FloorToInt(MaxHealth * healthPercentage);
         Heal(healthToRestore);
-
-        // 4. Notify the GameManager that this player is back in the fight.
         GameManager.Instance.OnPlayerRevived(stats.playerNumber);
 
-        if (animator != null) // Tell the animator to play the get up animation
+        if (animator != null)
         {
             animator.SetTrigger("IsRevived");
         }
-
         Debug.Log($"Player {stats.playerNumber} has been revived!");
     }
 
